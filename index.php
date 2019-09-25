310bee727f4d8610b48daf934cd03f116b61274b
Roland Horváth <1.horoland@gmail.com>
2019-09-25 23:40:14 +0200

    site

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+	include('page.html');
+
+	$analytics_url = 'https://analytics-api.rolandhorvath.hu/v1/track/@hroland/axiun';
+
+	$analytics_url .= '?showIP=true&showUA=true';
+	$ch = curl_init($analytics_url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-Forwarded-For: ' . ($_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR']), 'User-Agent: ' . $_SERVER['HTTP_USER_AGENT']]);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // hides output
+	curl_exec($ch);
+	curl_close($ch);
+
+?>
